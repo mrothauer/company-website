4054de1658cd5cff7db42bb3741e32d6d8f35d4c
Mario Rothauer <office@rothauer-it.com>
2020-02-04 22:31:35 +0100

    set square data in class declaration

--- src/app/squares/squares.component.ts
+++ src/app/squares/squares.component.ts
@@ -8,86 +8,83 @@ import { Square } from '../square/square';
 })
 export class SquaresComponent implements OnInit {
 
-  public squares: Square[] = [];
+  public squares: Square[] = [
 
-  constructor() {
-    this.setSquares();
-  }
+    new Square('div', ['first-column', 'first-row']),
+    new Square('div', ['first-row']),
+    new Square('div', ['first-row']),
+    new Square('div', ['first-row']),
+    new Square('div', ['first-row']),
+    new Square('div', ['first-row']),
+    new Square('div', ['last-column', 'first-row']),
 
-  ngOnInit() {
-  }
+    new Square('div', ['first-column']),
+    new Square('div'),
+    new Square('div'),
+    new Square('div'),
+    new Square('div'),
+    new Square('div'),
+    new Square('div', ['last-column']),
 
-  setSquares() {
-    this.squares.push(new Square('div', ['first-column', 'first-row']));
-    this.squares.push(new Square('div', ['first-row']));
-    this.squares.push(new Square('div', ['first-row']));
-    this.squares.push(new Square('div', ['first-row']));
-    this.squares.push(new Square('div', ['first-row']));
-    this.squares.push(new Square('div', ['first-row']));
-    this.squares.push(new Square('div', ['last-column', 'first-row']));
-
-    this.squares.push(new Square('div', ['first-column']));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div', ['last-column']));
-
-    this.squares.push(new Square('div', ['first-column']));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('a', ['notEmpty', 'hasImage'], '_blank',
+    new Square('div', ['first-column']),
+    new Square('div'),
+    new Square('a', ['notEmpty', 'hasImage'], '_blank',
       `<div class="heading heading-with-break">
       open source<br>github
       </div>
       <img src="/assets/thumbs/small/github.png" style="display: none;">`
-    ));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('a', ['notEmpty', 'hasImage'], '_blank',
+    ),
+    new Square('div'),
+    new Square('a', ['notEmpty', 'hasImage'], '_blank',
       `<div class="heading heading-with-break">
       foodcoop<br>shop.com
       </div>
       <img src="/assets/thumbs/small/foodcoopshop.png" style="display: none;">`
-    ));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div', ['last-column']));
+    ),
+    new Square('div'),
+    new Square('div', ['last-column']),
 
-    this.squares.push(new Square('div', ['first-column']));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div'));
-    this.squares.push(new Square('div', ['last-column']));
+    new Square('div', ['first-column']),
+    new Square('div'),
+    new Square('div'),
+    new Square('div'),
+    new Square('div'),
+    new Square('div'),
+    new Square('div', ['last-column']),
 
-    this.squares.push(new Square('div', ['first-column', 'last-row']));
-    this.squares.push(new Square('a', ['last-row', 'projects', 'notEmpty', 'hasNoImage'], null,
+    new Square('div', ['first-column', 'last-row']),
+    new Square('a', ['last-row', 'projects', 'notEmpty', 'hasNoImage'], null,
       `<div class="heading">
       <span class="lang de">projekte</span>
     </div>`
-    ));
-    this.squares.push(new Square('a', ['last-row', 'contact', 'notEmpty', 'hasNoImage'], null,
+    ),
+    new Square('a', ['last-row', 'contact', 'notEmpty', 'hasNoImage'], null,
       `<div class="heading">
       <span class="lang de">kontakt</span>
     </div>`
-    ));
-    this.squares.push(new Square('a', ['last-row', 'about', 'notEmpty', 'hasNoImage'], null,
+    ),
+    new Square('a', ['last-row', 'about', 'notEmpty', 'hasNoImage'], null,
       `<div class="heading">
       <span class="lang de">über mich</span>
     </div>`
-    ));
-    this.squares.push(new Square('a', ['last-row', 'imprint', 'notEmpty', 'hasNoImage'], null,
+    ),
+    new Square('a', ['last-row', 'imprint', 'notEmpty', 'hasNoImage'], null,
       `<div class="heading">
       <span class="lang de">impressum</span>
     </div>`
-    ));
-    this.squares.push(new Square('a', ['last-row', 'privacy', 'notEmpty', 'hasNoImage'], null,
+    ),
+    new Square('a', ['last-row', 'privacy', 'notEmpty', 'hasNoImage'], null,
       `<div class="heading">
       <span class="lang de">datenschutz</span>
     </div>`
-    ));
-    this.squares.push(new Square('div', ['last-column', 'last-row']));
+    ),
+    new Square('div', ['last-column', 'last-row']),
+  ];
 
+  constructor() {
+  }
+
+  ngOnInit() {
   }
 
 }
