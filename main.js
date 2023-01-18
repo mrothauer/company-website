f515cade7471b02a1708afea7b34310447409c51
mrothauer <office@rothauer-it.com>
2023-01-18 12:09:39 +0100

    restructured projects

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ import $ from 'jquery';
 window.$ = $;
 
 import Helper from './js/helper.js'
-import Projects from './js/projects.js'
+import projects from './js/projects.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 createApp(App).mount('#app')
 
-let projects = Projects.getAll()
 Helper.render(projects)
