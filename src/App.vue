96ea62a5e2b0bddb038b3b5cecedf07491ef142d
mrothauer <office@rothauer-it.com>
2023-02-02 21:26:47 +0100

    add projects.vue

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,7 @@
   import LanguageToggle from './components/LanguageToggle.vue'
   import Squares from './components/Squares.vue'
   
-  import FoodCoopShop from './components/projects/FoodCoopShop.vue'
-  import DorfladenOnline from './components/projects/DorfladenOnline.vue'
-  import HofladenOnline from './components/projects/HofladenOnline.vue'
-  import ReparaturInitiativen from './components/projects/ReparaturInitiativen.vue'
-  import Wildniscamps from './components/projects/Wildniscamps.vue'
-  import Fairteiler from './components/projects/Fairteiler.vue'
-  import OffeneWerkstaetten from './components/projects/OffeneWerkstaetten.vue'
-  import Motorsagla from './components/projects/Motorsagla.vue'
-
+  import Projects from './components/pages/Projects.vue'
   import About from './components/pages/About.vue'
   import Contact from './components/pages/Contact.vue'
   import Privacy from './components/pages/Privacy.vue'
@@ -37,17 +29,7 @@
 
     <div id="desc" class="description square">
       
-      <div class="desc" id="projects">
-        <FoodCoopShop />
-        <DorfladenOnline />
-        <HofladenOnline />
-        <ReparaturInitiativen />
-        <Wildniscamps />
-        <Fairteiler />
-        <OffeneWerkstaetten />
-        <Motorsagla />
-      </div>
-      
+      <Projects />
       <About />
       <Contact />
       <Privacy />
---
+++ src/components/pages/Projects.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+  import FoodCoopShop from '../projects/FoodCoopShop.vue'
+  import DorfladenOnline from '../projects/DorfladenOnline.vue'
+  import HofladenOnline from '../projects/HofladenOnline.vue'
+  import ReparaturInitiativen from '../projects/ReparaturInitiativen.vue'
+  import Wildniscamps from '../projects/Wildniscamps.vue'
+  import Fairteiler from '../projects/Fairteiler.vue'
+  import OffeneWerkstaetten from '../projects/OffeneWerkstaetten.vue'
+  import Motorsagla from '../projects/Motorsagla.vue'
+
+</script>
+
+<template>
+    <div class="desc" id="projects">
+        <FoodCoopShop />
+        <DorfladenOnline />
+        <HofladenOnline />
+        <ReparaturInitiativen />
+        <Wildniscamps />
+        <Fairteiler />
+        <OffeneWerkstaetten />
+        <Motorsagla />
+    </div>
+</template>
