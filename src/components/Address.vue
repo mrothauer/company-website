d92412138e0ad8b18785caa2af22a79e6844a1d8
mrothauer <office@rothauer-it.com>
2026-04-24 22:33:04 +0200

    obfuscate

--- src/components/Address.vue
+++ src/components/Address.vue
@@ -9,7 +9,24 @@
         A-4644 Grünau im Almtal
     </p>
     <p>+43 680 217 89 39<br />
-        <a href="mailto:office@rothauer-it.com">office@rothauer-it.com</a><br />
+        <a :href="mailHref" @click="revealMail" @mouseover="revealMail" @focus="revealMail">
+            <span v-html="mailText"></span>
+        </a><br />
         UID-Nr. ATU71726279<br />
     </p>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const user = 'office'
+const domain = 'rothauer-it.com'
+const obfuscated = `${user}&#64;${domain.replace('.', '&#46;')}`
+const mailText = ref(obfuscated)
+const mailHref = ref('#')
+
+function revealMail() {
+    mailHref.value = `mailto:${user}@${domain}`
+    mailText.value = `${user}@${domain}`
+}
+</script>
